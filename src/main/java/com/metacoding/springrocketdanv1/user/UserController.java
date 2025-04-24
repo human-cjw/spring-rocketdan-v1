801d40d9892d16c75752cf258170844a8385cb9c
YeounhoLee <yh88888888@gmail.com>
2025-04-24 16:27:34 +0900

    로그인

--- src/main/java/com/metacoding/springrocketdanv1/user/UserController.java
+++ src/main/java/com/metacoding/springrocketdanv1/user/UserController.java
@@ -1,5 +1,8 @@
 package com.metacoding.springrocketdanv1.user;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +12,40 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final HttpSession session;
+
+
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO loginDTO, HttpServletResponse response) {
+        User sessionUser = userService.로그인(loginDTO);
+        session.setAttribute("sessionUser", sessionUser);
+        if(loginDTO.getRememberMe()==null){
+            Cookie cookie = new Cookie("username",null);
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+        }else {
+            Cookie cookie = new Cookie ("username", loginDTO.getUsername());
+            cookie.setMaxAge(60*60*24*7);
+            response.addCookie(cookie);
+        }
+        return "job/list";
+    }
+
+    @GetMapping("/login-form")
+    public String loginForm() {
+        return "user/login-form";
+    }
+
+    @PostMapping("/join")
+    public String join(UserRequest.JoinDTO joinDTO) {
+        userService.회원가입(joinDTO);
+        return "redirect:/login-form";
+    }
 
     @GetMapping("/join-form")
     public String joinform() {
         return "user/join-form";
     }
-    @PostMapping("/join")
-    public String join(UserRequest.JoinDTO joinDTO) {
-        userService.회원가입(joinDTO);
-        return "user/login-form";
-    }
+
+
 }
--- src/main/java/com/metacoding/springrocketdanv1/user/UserRequest.java
+++ src/main/java/com/metacoding/springrocketdanv1/user/UserRequest.java
@@ -21,4 +21,11 @@ public class UserRequest {
         }
     }
 
+    @Data
+    public static class LoginDTO {
+        private String username;
+        private String password;
+        private String rememberMe;
+        }
+
 }
