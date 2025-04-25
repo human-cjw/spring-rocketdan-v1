eb9a2e90a0d18b3989fdcd02d12fddb51c097faf
YeounhoLee <yh88888888@gmail.com>
2025-04-25 16:30:49 +0900

    구직자목록보기

--- src/test/java/com/metacoding/springrocketdanv1/resume/ResumeRepositoryTest.java
+++ src/test/java/com/metacoding/springrocketdanv1/resume/ResumeRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.metacoding.springrocketdanv1.resume;
 
+import jakarta.persistence.Query;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Import(ResumeRepository.class)
 @DataJpaTest
@@ -22,4 +26,15 @@ public class ResumeRepositoryTest {
         // eye
         System.out.println("출력 : " + resume);
     }
+
+    @Test
+    public void findAll_test() {
+
+
+        List<Resume> resumes = resumeRepository.findAll();
+
+        System.out.println("출력 : " + resumes);
+    }
 }
+
+
