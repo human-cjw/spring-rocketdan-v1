56f6087d40faffc49b82906d2590ac7535d7e1ea
jjack-1 <qazwsx0088@gmail.com>
2025-04-27 23:47:00 +0900

    컨트롤러 연결

--- src/main/java/com/metacoding/springrocketdanv1/application/ApplicationController.java
+++ src/main/java/com/metacoding/springrocketdanv1/application/ApplicationController.java
@@ -1,10 +1,22 @@
 package com.metacoding.springrocketdanv1.application;
 
+import com.metacoding.springrocketdanv1.user.UserResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
 public class ApplicationController {
     private final ApplicationService applicationService;
+    private final HttpSession session;
+
+    @GetMapping("/user/job/{jobId}/apply-form")
+    public String applyForm(@PathVariable("jobId") Integer jobId) {
+        UserResponse.SessionUserDTO sessionUserDTO = (UserResponse.SessionUserDTO) session.getAttribute("sessionUser");
+
+        return "user/application/apply-form";
+    }
 }
