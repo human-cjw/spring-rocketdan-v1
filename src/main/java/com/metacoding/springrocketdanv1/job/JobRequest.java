ea675ca49837c3f062a0a3e7b613894da91559d8
jjack-1 <qazwsx0088@gmail.com>
2025-04-27 20:57:50 +0900

    job-update complete & job-save-v2 update

--- src/main/java/com/metacoding/springrocketdanv1/job/JobRequest.java
+++ src/main/java/com/metacoding/springrocketdanv1/job/JobRequest.java
@@ -25,10 +25,9 @@ public class JobRequest {
         private Integer workFieldId;
         private String careerLevel;
         private Integer salaryRangeId;
-        private Integer companyId; // 일단 mustache에서 받아옴. 나중에 sessionUser 에서 가져올 수 있음
-        private List<Integer> techStacks;
+        private List<Integer> techStackIds;
 
-        public Job toEntity() {
+        public Job toEntity(Integer companyId) {
             Job job = Job.builder()
                     .title(title)
                     .description(description)
@@ -51,8 +50,8 @@ public class JobRequest {
                             .build())
                     .build();
 
-            for (Integer techStackId : techStacks) {
-                job.getJobTechStackList().add(
+            for (Integer techStackId : techStackIds) {
+                job.getJobTechStacks().add(
                         JobTechStack.builder()
                                 .job(job)
                                 .techStack(TechStack.builder()
@@ -64,5 +63,20 @@ public class JobRequest {
 
             return job;
         }
+    }
+
+    @Data
+    public static class JobUpdateDTO {
+        private String title;
+        private String description;
+        private String location;
+        private String employmentType;
+        private String deadline;
+        private String status;
+        private Integer jobGroupId;
+        private Integer workFieldId;
+        private String careerLevel;
+        private Integer salaryRangeId;
+        private List<Integer> techStackIds;
     }
 }
