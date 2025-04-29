a7c860930685341a37c889cf3ce62b445896b105
jjack-1 <qazwsx0088@gmail.com>
2025-04-29 14:56:16 +0900

    feat/resume-list 완료

--- src/main/java/com/metacoding/springrocketdanv1/resume/ResumeRepository.java
+++ src/main/java/com/metacoding/springrocketdanv1/resume/ResumeRepository.java
@@ -1,8 +1,11 @@
 package com.metacoding.springrocketdanv1.resume;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -11,5 +14,20 @@ public class ResumeRepository {
 
     public Resume findById(Integer id) {
         return em.find(Resume.class, id);
+    }
+
+    public List<Resume> findAllByUserId(Integer userId, boolean isDefault) {
+        String sql;
+
+        if (isDefault) {
+            sql = "SELECT res FROM Resume res WHERE res.user.id = :userId and res.isDefault = true ORDER BY res.id DESC";
+
+        } else {
+            sql = "SELECT res FROM Resume res WHERE res.user.id = :userId ORDER BY res.id DESC";
+        }
+
+        Query query = em.createQuery(sql, Resume.class);
+        query.setParameter("userId", userId);
+        return query.getResultList();
     }
 }
