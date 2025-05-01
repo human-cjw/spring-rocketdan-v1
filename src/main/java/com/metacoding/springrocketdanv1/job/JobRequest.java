0caa8c04085c520785fcb4cbc7d61f2b32961d11
jjack-1 <qazwsx0088@gmail.com>
2025-05-01 14:18:57 +0900

    중간저장

--- src/main/java/com/metacoding/springrocketdanv1/job/JobRequest.java
+++ src/main/java/com/metacoding/springrocketdanv1/job/JobRequest.java
@@ -6,6 +6,7 @@ import com.metacoding.springrocketdanv1.jobTechStack.JobTechStack;
 import com.metacoding.springrocketdanv1.salaryRange.SalaryRange;
 import com.metacoding.springrocketdanv1.techStack.TechStack;
 import com.metacoding.springrocketdanv1.workField.WorkField;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.util.List;
@@ -14,8 +15,11 @@ public class JobRequest {
 
     @Data
     public static class JobSaveDTO {
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]{}\\/!@#$%^&*\\-+=~\\.,]{2,20}$", message = "제목은 최소 2자에서 최대 20자 입니다")
         private String title;
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]{}\\/!@#$%^&*\\-+=~\\.,]{2,300}$", message = "설명은 최소 2자에서 최대 300자 입니다")
         private String description;
+        @Pattern(regexp = "^^[가-힣]{2,20}\\s[가-힣]{1,10}\\s[가-힣]{1,20}$", message = "주소는 두 단어만 작성해 주세요. 예)서울특별시 구로구 디지털로")
         private String location;
         private String employmentType;
         private String deadline;
@@ -66,8 +70,11 @@ public class JobRequest {
 
     @Data
     public static class JobUpdateDTO {
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]{}\\/!@#$%^&*\\-+=~\\.,]{2,20}$", message = "제목은 최소 2자에서 최대 20자 입니다")
         private String title;
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]{}\\/!@#$%^&*\\-+=~\\.,]{2,300}$", message = "설명은 최소 2자에서 최대 300자 입니다")
         private String description;
+        @Pattern(regexp = "^[가-힣]{2,20}\\s[가-힣]{1,10}\\s[가-힣]{1,20}$", message = "주소는 세 단어만 작성해 주세요. 예)서울특별시 구로구 디지털로")
         private String location;
         private String employmentType;
         private String deadline;
--- src/main/java/com/metacoding/springrocketdanv1/resume/ResumeRequest.java
+++ src/main/java/com/metacoding/springrocketdanv1/resume/ResumeRequest.java
@@ -5,6 +5,7 @@ import com.metacoding.springrocketdanv1.resumeTechStack.ResumeTechStack;
 import com.metacoding.springrocketdanv1.salaryRange.SalaryRange;
 import com.metacoding.springrocketdanv1.techStack.TechStack;
 import com.metacoding.springrocketdanv1.user.User;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.util.List;
@@ -13,24 +14,34 @@ public class ResumeRequest {
 
     @Data
     public static class UpdateDTO {
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]{}\\/!@#$%^&*\\-+=~\\.,]{2,20}$", message = "제목은 최소 2자에서 최대 20자 입니다")
         private String title; // 제목
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]{}\\/!@#$%^&*\\-+=~\\.,]{2,300}$", message = "설명은 최소 2자에서 최대 300자 입니다")
         private String summary; // 자기소개
         private String portfolioUrl; // 포트폴리오 주소
         private String gender; // 성별
         private String education; // 학력사항
         private String birthdate; // 생년월일
+        @Pattern(regexp = "^[가-힣]+(?:\\s[가-힣]+){2}$", message = "한글만 가능합니다")
         private String major; // 전공
+        @Pattern(regexp = "^(졸업|재학|휴학|졸업예정)$", message = "졸업|재학|휴학|졸업예정 중 하나를 넣어주세요")
         private String graduationType; // 졸업, 재학, 휴학
+        @Pattern(regexp = "^0\\d{9,10}$\n", message = "하이픈 없이 숫자만 넣어주세요")
         private String phone; // 연락처
         private String enrollmentDate; // 입학날짜
         private String graduationDate; // 졸업날짜
+        @Pattern(regexp = "^(경력|신입)$", message = "경력|신입 중 하나를 넣어주세요")
         private String careerLevel;
         private Boolean isDefault;
+        @Pattern(regexp = "^[가-힣]+(?:\\s[가-힣]+){2}$", message = "한글만 가능합니다")
         private String careerCompanyName;
         private String careerStartDate;
         private String careerEndDate;
+        @Pattern(regexp = "^[가-힣]+(?:\\s[가-힣]+){2}$", message = "한글만 가능합니다")
         private String certificationName;
+        @Pattern(regexp = "^[가-힣]+(?:\\s[가-힣]+){2}$", message = "한글만 가능합니다")
         private String certificationIssuer;
+        @Pattern(regexp = "^[가-힣]+(?:\\s[가-힣]+){2}$", message = "한글만 가능합니다")
         private String certificationIssuedDate;
         private Integer salaryRangeId;
         private Integer jobGroupId;
